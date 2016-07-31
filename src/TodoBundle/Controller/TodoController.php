d74299444f24a79bc23aaf2b8e066155f0a28433
AlexBordo <alexbursak3@gmail.com>
2016-07-31 21:57:52 +0100

    Changed view file tree, updated TodoController, routing

--- src/TodoBundle/Controller/TodoController.php
+++ src/TodoBundle/Controller/TodoController.php
@@ -3,49 +3,52 @@
 namespace TodoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class TodoController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TodoBundle:Todo:index.html.twig', array(
+        return $this->render('TodoBundle:Main:index.html.twig', array(
             // ...
         ));
     }
 
     public function listAllAction()
     {
-        return $this->render('TodoBundle:Todo:list_all.html.twig', array(
-            // ...
+        $todos = $this->getDoctrine()
+            ->getRepository('TodoBundle:Todo')
+            ->findAll();
+        
+        return $this->render('TodoBundle:Todo/pages:list_all.html.twig', array(
+            'todos' => $todos
         ));
     }
 
     public function showAction()
     {
-        return $this->render('TodoBundle:Todo:show.html.twig', array(
+        return $this->render('TodoBundle:Todo/pages:show.html.twig', array(
             // ...
         ));
     }
 
     public function createAction()
     {
-        return $this->render('TodoBundle:Todo:create.html.twig', array(
+        return $this->render('TodoBundle:Todo/pages:create.html.twig', array(
             // ...
         ));
     }
 
     public function editAction()
     {
-        return $this->render('TodoBundle:Todo:edit.html.twig', array(
+        return $this->render('TodoBundle:Todo/pages:edit.html.twig', array(
             // ...
         ));
     }
 
     public function deleteAction()
     {
-        return $this->render('TodoBundle:Todo:delete.html.twig', array(
-            // ...
-        ));
+        return new Response('<htlm><body>DELETION PAGE</body></htlm>');
     }
 
 }
